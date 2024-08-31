cbaccc5c1d7e7cd77c42de926e33fc3af761c213
Shelbyen <ars.sheberstov@mail.ru>
2024-08-31 16:13:43 +0500

    add: base structure

--- src/main/kotlin/funn/j2k/logserver/Main.kt
+++ src/main/kotlin/funn/j2k/logserver/Main.kt
@@ -1,5 +1,30 @@
 package funn.j2k.logserver
 
-fun main() {
-    println("Hello World!")
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val selectorManager = SelectorManager(Dispatchers.IO)
+    val localAddress = InetSocketAddress("127.0.0.1", 54527)
+    val socket = aSocket(selectorManager).udp().connect(localAddress)
+
+    while (true) {
+        launch {
+            val receiveChannel = socket.openReadChannel()
+
+            try {
+                while (true) {
+                    val newString = receiveChannel.readUTF8Line()
+                    println(newString)
+                }
+            } catch (e: Throwable) {
+                println(e.message)
+                socket.close()
+            }
+        }
+    }
 }
